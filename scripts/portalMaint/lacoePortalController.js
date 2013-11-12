5df8db826bc51d5ee6ccd5c3511e0e4ea418d2d7
John D Sanders <john@seitmc.com>
2013-11-12 08:17:53 -0800

    Initial Load

---
+++ scripts/portalMaint/lacoePortalController.js
@@ -0,0 +1,94 @@
+﻿'use strict'
+/* 
+    Written By: John D Sanders
+    Last Update: 11/9/2013
+    Description: Angular/Jquery portal template with drag and drop portlets.
+*/
+var lacoePortal = angular.module("lacoePortal", []);
+var objPortlet = [];
+
+lacoePortal.controller('MainController', ['$scope', function ($scope) {
+    $scope.objPortlet = [
+        {
+            portletName: "EPIC",
+            portletTop: 0,
+            portletLeft: 0,
+            portletType: "Small",
+            portletUrl: 'http://www.seitmc.com',
+            portletImage: 'http://www.placehold.it/144x100&text=graphic',
+            portletMessage: 'Epic SIS'
+        },
+        {
+            portletName: "SPED4",
+            portletTop: -155,
+            portletLeft: 465,
+            portletType: "Small",
+            portletUrl: 'http://www.seitmc.com',
+            portletImage: 'http://www.placehold.it/144x100&text=graphic',
+            portletMessage: ''
+        },
+        {
+            portletName: "PR Tool",
+            portletTop: -155,
+            portletLeft: 155,
+            portletType: "Large",
+            portletUrl: 'http://www.seitmc.com',
+            portletImage: '',
+            portletMessage: 'This is the PR Tool. Click anywhere on the portlet to access the tool.'
+        },
+        {
+            portletName: "ILP",
+            portletTop: -310,
+            portletLeft: 465,
+            portletType: "Small",
+            portletUrl: 'http://www.seitmc.com',
+            portletImage: 'http://www.placehold.it/144x100&text=graphic',
+            portletMessage: ''
+        },
+        {
+            portletName: "FRM",
+            portletTop: -465,
+            portletLeft: 0,
+            portletType: "Small",
+            portletUrl: 'http://www.seitmc.com',
+            portletImage: 'http://www.placehold.it/144x100&text=graphic',
+            portletMessage: 'Free and Reduced Meals'
+        },
+        {
+            portletName: "HRIS",
+            portletTop: -465,
+            portletLeft: 155,
+            portletType: "Large",
+            portletUrl: 'http://www.seitmc.com',
+            portletImage: '',
+            portletMessage: 'Human Resources System'
+        },
+        {
+            portletName: "ANY 1",
+            portletTop: -620,
+            portletLeft: 0,
+            portletType: "Small",
+            portletUrl: 'http://www.seitmc.com',
+            portletImage: 'http://www.placehold.it/144x100&text=graphic',
+            portletMessage: ''
+        },
+        {
+            portletName: "ANY 2",
+            portletTop: -775,
+            portletLeft: 465,
+            portletType: "Small",
+            portletUrl: 'http://www.seitmc.com',
+            portletImage: 'http://www.placehold.it/144x100&text=graphic',
+            portletMessage: ''
+        },
+        {
+            portletName: "ANY 3",
+            portletTop: -1240,
+            portletLeft: 155,
+            portletType: "Large",
+            portletUrl: 'http://www.seitmc.com',
+            portletImage: '',
+            portletMessage: ''
+        },
+    ];
+}]);
---
+++ scripts/portalMaint/portalScripts.js
@@ -0,0 +1,23 @@
+﻿/* 
+    Written By: John D Sanders
+    Last Update: 11/9/2013
+    Description: Angular/Jquery portal template with drag and drop portlets.
+*/
+$(document).ready(function () {
+    $("[class^=moveablePortlet]")
+        .draggable({
+            grid: [155, 155],
+            containment: '#portalArea'
+        })
+        .mouseover(function (event) {
+            console.log($(event.target).attr("data-portletName"));
+        });
+        //.resizable({
+        //    grid: 155,
+        //    containment: '#portalArea'
+    //});
+});
+
+$("[class^=moveablePortlet]").click(function (event) {
+    //alert("clicked" + event.type);
+});
