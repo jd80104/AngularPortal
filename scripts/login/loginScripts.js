e214020b0770ad23649936c39a522a42b5e9a891
John D Sanders <john@seitmc.com>
2013-11-12 11:45:20 -0800

    Added Login Page Template

---
+++ scripts/login/loginScripts.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $('.cancel').click(function () {
+
+        $('#loginForm').removeClass();
+        $('#loginForm').addClass('animated shake');
+    });
+
+    $('.submit').click(function () {
+
+        $('#loginForm').removeClass();
+        $('#loginForm').addClass('animated flipOutY');
+        window.location.replace('index.html')
+    });
+});
