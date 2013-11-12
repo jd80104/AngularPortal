b8b78110d8d7872733eea16edaa0b2eb87c16009
John D Sanders <john@seitmc.com>
2013-11-12 11:49:53 -0800

    added behaviors for login page

--- scripts/login/loginScripts.js
+++ scripts/login/loginScripts.js
@@ -1,14 +1,24 @@
 ﻿$(document).ready(function () {
     $('.cancel').click(function () {
-
         $('#loginForm').removeClass();
         $('#loginForm').addClass('animated shake');
+        window.close();
     });
 
     $('.submit').click(function () {
-
+        var isValid = false;
         $('#loginForm').removeClass();
         $('#loginForm').addClass('animated flipOutY');
-        window.location.replace('index.html')
+
+        // Call the backend and set isValid
+        isValid = true;
+
+        if (isValid) {
+            window.location.replace('index.html')
+        }
+        else {
+            $('#loginForm').removeClass();
+            $('#loginForm').addClass('animated shake');
+        }
     });
 });
